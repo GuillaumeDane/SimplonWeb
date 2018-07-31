33c6b77fedd43c310d8a6a2e6698108918b63323
GuillaumeDane <daneguillaume.prod@gmail.com>
2018-07-31 17:44:32 +0200

    +

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems, options);
-  });
+$(document).ready(function(){
+  $('.slider').slider();
+});
